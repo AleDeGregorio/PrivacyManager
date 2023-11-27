341b2882415171b856bcefce795a84895b656b26
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-11-27 10:36:12 +0100

    feat: violation and notification activities

---
+++ app/src/main/java/it/polito/s294545/privacymanager/NotificationActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.s294545.privacymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
---
+++ app/src/main/java/it/polito/s294545/privacymanager/ViolationActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.s294545.privacymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViolationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_violation)
+    }
+}
