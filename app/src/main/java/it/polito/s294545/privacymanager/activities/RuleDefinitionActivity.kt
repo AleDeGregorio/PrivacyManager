816cf92cceee795102dee4d80e1564cc2abdb6cf
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-12-07 16:51:07 +0100

    feat: viewpager animation

--- app/src/main/java/it/polito/s294545/privacymanager/activities/RuleDefinitionActivity.kt
+++ app/src/main/java/it/polito/s294545/privacymanager/activities/RuleDefinitionActivity.kt
@@ -116,6 +116,7 @@ class RuleDefinitionActivity : AppCompatActivity(), ParameterListener {
 
         // Define rule parameters fragments
         viewPager = findViewById(R.id.view_pager)
+        viewPager.setPageTransformer(ZoomOutPageTransformer())
         val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
 
         fragmentList = mutableListOf(
@@ -424,4 +425,48 @@ class FormPagerAdapter(activity: FragmentActivity, private val pages: List<Fragm
     override fun getItemCount(): Int = pages.size
 
     override fun createFragment(position: Int): Fragment = pages[position]
+}
+
+// ViewPager animation
+
+private const val MIN_SCALE = 0.85f
+private const val MIN_ALPHA = 0.5f
+
+class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+
+    override fun transformPage(view: View, position: Float) {
+        view.apply {
+            val pageWidth = width
+            val pageHeight = height
+            when {
+                position < -1 -> { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    alpha = 0f
+                }
+                position <= 1 -> { // [-1,1]
+                    // Modify the default slide transition to shrink the page as well.
+                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                    val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                    val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                    translationX = if (position < 0) {
+                        horzMargin - vertMargin / 2
+                    } else {
+                        horzMargin + vertMargin / 2
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1).
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+
+                    // Fade the page relative to its size.
+                    alpha = (MIN_ALPHA +
+                            (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                }
+                else -> { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    alpha = 0f
+                }
+            }
+        }
+    }
 }
