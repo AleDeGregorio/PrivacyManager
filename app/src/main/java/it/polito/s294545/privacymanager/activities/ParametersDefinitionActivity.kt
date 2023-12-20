32c53de56bccb702317a4dc82af92309066e05a5
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-12-20 11:14:45 +0100

    starting new parameters definition activity

---
+++ app/src/main/java/it/polito/s294545/privacymanager/activities/ParametersDefinitionActivity.kt
@@ -0,0 +1,12 @@
+package it.polito.s294545.privacymanager.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.polito.s294545.privacymanager.R
+
+class ParametersDefinitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parameters_definition)
+    }
+}
