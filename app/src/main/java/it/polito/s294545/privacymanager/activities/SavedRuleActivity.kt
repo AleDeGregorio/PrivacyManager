17427a05becce14339745c34bd3bed94faaf4c6b
AleDeGregorio <alessio.degregorio.98@gmail.com>
2023-12-27 12:53:55 +0100

    feat: saved rule

--- app/src/main/java/it/polito/s294545/privacymanager/activities/SavedRuleActivity.kt
+++ app/src/main/java/it/polito/s294545/privacymanager/activities/SavedRuleActivity.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
-import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.widget.Button
@@ -60,46 +58,53 @@ class SavedRuleActivity : AppCompatActivity() {
         ruleName.text = rule.name
 
         // Set rule permissions
-        val permissions = findViewById<TextView>(R.id.permissions_text)
+        val permissions = findViewById<TextView>(R.id.list_permissions)
         permissions.text = getPermissionsString()
 
         // Set rule applications
-        val apps = findViewById<TextView>(R.id.apps_text)
+        val apps = findViewById<TextView>(R.id.list_apps)
         apps.text = getAppsString()
 
         // Set rule action
         val action = findViewById<TextView>(R.id.action_text)
         action.text = getActionString()
 
-        // Set rule positions
-        val positionsCard = findViewById<MaterialCardView>(R.id.positions_card)
-        if (rule.positions.isNullOrEmpty()) {
-            positionsCard.visibility = GONE
-        }
-        else {
-            val positions = findViewById<TextView>(R.id.positions_text)
-            positions.text = getPositionString()
-        }
+        // Set conditions
+        val conditionsCard = findViewById<MaterialCardView>(R.id.conditions_card)
+        if (rule.positions.isNullOrEmpty() && (rule.timeSlot == null || rule.timeSlot!!.days.isEmpty()) &&
+            rule.networks.isNullOrEmpty() && rule.bt.isNullOrEmpty() && rule.battery == null) {
 
-        // Set rule time slot
-        val timeSlotCard = findViewById<MaterialCardView>(R.id.time_slot_card)
-        if (rule.timeSlot == null || rule.timeSlot!!.days.isEmpty()) {
-            timeSlotCard.visibility = GONE
+            conditionsCard.visibility = GONE
         }
         else {
+            // Set time slot
+            val timeSlotTitle = findViewById<TextView>(R.id.time_slot_title)
             val timeSlot = findViewById<TextView>(R.id.time_slot_text)
-            timeSlot.text = getTimeSlotString()
-        }
 
-        // Set rule parameters
-        val parametersCard = findViewById<MaterialCardView>(R.id.parameters_card)
-        if (rule.networks.isNullOrEmpty() && rule.bt.isNullOrEmpty() && rule.battery == null) {
-            parametersCard.visibility = GONE
-        }
-        else {
-            // Set rule networks
+            if (rule.timeSlot == null || rule.timeSlot!!.days.isEmpty()) {
+                timeSlotTitle.visibility = GONE
+                timeSlot.visibility = GONE
+            }
+            else {
+                timeSlot.text = getTimeSlotString()
+            }
+
+            // Set positions
+            val positionsTitle = findViewById<TextView>(R.id.positions_title)
+            val positions = findViewById<TextView>(R.id.positions_text)
+
+            if (rule.positions.isNullOrEmpty()) {
+                positionsTitle.visibility = GONE
+                positions.visibility = GONE
+            }
+            else {
+                positions.text = getPositionString()
+            }
+
+            // Set networks
             val networksTitle = findViewById<TextView>(R.id.networks_title)
             val networks = findViewById<TextView>(R.id.networks_text)
+
             if (rule.networks.isNullOrEmpty()) {
                 networksTitle.visibility = GONE
                 networks.visibility = GONE
@@ -108,9 +113,10 @@ class SavedRuleActivity : AppCompatActivity() {
                 networks.text = getNetworkString()
             }
 
-            // Set rule bluetooth
+            // Set bluetooth
             val bluetoothTitle = findViewById<TextView>(R.id.bluetooth_title)
             val bluetooth = findViewById<TextView>(R.id.bluetooth_text)
+
             if (rule.bt.isNullOrEmpty()) {
                 bluetoothTitle.visibility = GONE
                 bluetooth.visibility = GONE
@@ -119,9 +125,10 @@ class SavedRuleActivity : AppCompatActivity() {
                 bluetooth.text = getBluetoothString()
             }
 
-            // Set rule battery
+            // Set battery
             val batteryTitle = findViewById<TextView>(R.id.battery_title)
             val battery = findViewById<TextView>(R.id.battery_text)
+
             if (rule.battery == null) {
                 batteryTitle.visibility = GONE
                 battery.visibility = GONE
@@ -130,15 +137,6 @@ class SavedRuleActivity : AppCompatActivity() {
                 battery.text = getBatteryString()
             }
         }
-
-        // Manage start and stop rule
-        val startStopRuleButton = findViewById<FloatingActionButton>(R.id.start_rule_button)
-
-        if (rule.active) {
-            startStopRuleButton.setImageDrawable(resources.getDrawable(R.drawable.icon_stop))
-        }
-
-        startStopRuleButton.setOnClickListener { v -> showPopupStartStopRule(v) }
     }
 
     private fun manageBackNavigation() {
@@ -190,7 +188,7 @@ class SavedRuleActivity : AppCompatActivity() {
         val positionsStr = StringBuilder("")
 
         for (p in rule.positions!!) {
-            positionsStr.append("- ${p.address}.\n\n")
+            positionsStr.append("- ${p.address}.\n")
         }
 
         val tmp = positionsStr.toString()
@@ -244,50 +242,6 @@ class SavedRuleActivity : AppCompatActivity() {
     }
 
     private fun getBatteryString() : String {
-        return "< ${rule.battery}%"
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    fun showPopupStartStopRule(view: View) {
-        val (popupView, popupWindow) = managePopup(view, R.layout.popup_start_rule)
-
-        // Initialize the elements of our window, install the handler
-        val title = popupView.findViewById<TextView>(R.id.title)
-        val buttonStartStopRule = popupView.findViewById<Button>(R.id.start_rule_button)
-        val buttonCancel = popupView.findViewById<Button>(R.id.cancel_button)
-
-        if (rule.active) {
-            title.text = "Disattivare la regola?"
-            buttonStartStopRule.text = "Disattiva"
-        }
-
-        buttonStartStopRule.setOnClickListener {
-            // Change rule state
-            rule.active = !rule.active
-
-            // Update rule in shared preferences
-            // Convert rule object to JSON string
-            val ruleJSON = Json.encodeToString(Rule.serializer(), rule)
-
-            // Save privacy rule in shared preferences
-            PreferencesManager.savePrivacyRule(this, rule.name!!, ruleJSON)
-
-            // Navigate back to homepage
-            val intent = Intent(this@SavedRuleActivity, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            startActivity(intent)
-            finish()
-        }
-
-        buttonCancel.setOnClickListener {
-            popupWindow.dismiss()
-        }
-
-
-        // Handler for clicking on the inactive zone of the window
-        popupView.setOnTouchListener { v, event -> // Close the window when clicked
-            popupWindow.dismiss()
-            true
-        }
+        return "<${rule.battery}%"
     }
 }
